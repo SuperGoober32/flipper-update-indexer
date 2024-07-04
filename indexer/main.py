93aae63d838fbe54fe07a8bd14fb17800c9f6873
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-07-04 14:13:35 +0200

    Serve only on localhost by default

--- indexer/main.py
+++ indexer/main.py
@@ -70,7 +70,7 @@ def main() -> None:
         logger.addHandler(handler)
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=settings.port,
         workers=settings.workers,
         log_config=None,
